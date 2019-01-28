795a8e5746bfce1b9d7d7e6093097e83116bd6aa
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-01-28 09:28:53 +0530

    [WIP]: implementing routing for different timeline

--- src/navbar/navbarComponent.js
+++ src/navbar/navbarComponent.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'
 import './navbar.css'
 
 
 class NavBarComponent extends Component {
   render() {
-    return <nav class='Navbar'></nav>
+    return <div className='Navbar'>
+      <NavLink to="/bio" className="Nav-element" activeClassName="Selected">
+        Bio
+      </NavLink>
+      <NavLink to="/projects" className="Nav-element" activeClassName="Selected">
+        Projects
+      </NavLink>
+      <NavLink to="/experience" className="Nav-element" activeClassName="Selected">
+        Experience
+      </NavLink>
+      <NavLink to="/blog" className="Nav-element" activeClassName="Selected">
+        Blog
+      </NavLink>
+
+    </div>
   }
 }
 
