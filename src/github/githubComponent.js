6d27f6b39ece6096231c65de3903b8ae1b357b12
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-02-06 08:05:55 +0530

    [Wip]: making truly responsive for mobile

--- src/github/githubComponent.js
+++ src/github/githubComponent.js
@@ -6,27 +6,28 @@ class GithubComponent extends Component {
   state = {
     commits: []
   }
-  componentDidMount() {
-    axios.get('https://api.github.com/users/vkbinfo/events').then((eventData) => {
-      console.log(eventData);
-      let commitsInLastThreePushEvent = eventData.data.slice(5, 9).reduce((acc, event) => acc.concat(event.payload.commits.map(commit => {
-        let commitDiffUrl = `https://github.com/${event.repo.name}/commit/${commit.sha}`;
-        let repoName = event.repo.name.split('/')[1] // the format is username/reponame so I am splitting it.
-        let timeAfterPush = Date.now() - Date.parse(event.created_at);
-        return { ...commit, url: commitDiffUrl, timeAfterPush, repoName }
-      })), []);
-      let commits = commitsInLastThreePushEvent.sort((commitA, commitB) => commitA.timeAfterPush > commitB.timeAfterPush).map(commit => {
-        let commitMessage = commit.message;
-        let commitUrl = commit.url;
-        let timeAfterPush = commit.timeAfterPush;
-        let minutes = Math.round((timeAfterPush % (1000 * 60 * 60)) / (1000 * 60))
-        let hours = Math.round(timeAfterPush / (1000 * 60 * 60));
-        let timeAfterPushString = `${hours} hours and ${minutes} minutes ago in `
-        return { commitMessage, commitUrl, timeAfterPushString, repoName: commit.repoName };
-      }).slice(0, 4); // Only showing 4 commit messages.
-      this.setState({ commits })
-    })
-  }
+  // componentDidMount() {
+  //   axios.get('https://api.github.com/users/vkbinfo/events').then((eventData) => {
+  //     console.log(eventData);
+  //     let commitsInLastThreePushEvent = eventData.data.slice(5, 9).reduce((acc, event) => acc.concat(event.payload.commits.map(commit => {
+  //       let commitDiffUrl = `https://github.com/${event.repo.name}/commit/${commit.sha}`;
+  //       let repoName = event.repo.name.split('/')[1] // the format is username/reponame so I am splitting it.
+  //       let timeAfterPush = Date.now() - Date.parse(event.created_at);
+  //       return { ...commit, url: commitDiffUrl, timeAfterPush, repoName }
+  //     })), []);
+  //     let commits = commitsInLastThreePushEvent.sort((commitA, commitB) => commitA.timeAfterPush > commitB.timeAfterPush).map(commit => {
+  //       let commitMessage = commit.message;
+  //       let commitUrl = commit.url;
+  //       let timeAfterPush = commit.timeAfterPush;
+  //       let minutes = Math.round((timeAfterPush % (1000 * 60 * 60)) / (1000 * 60))
+  //       let hours = Math.round(timeAfterPush / (1000 * 60 * 60));
+  //       let timeAfterPushString = `${hours} hours and ${minutes} minutes ago in `
+  //       return { commitMessage, commitUrl, timeAfterPushString, repoName: commit.repoName };
+  //     }).slice(0, 4); // Only showing 4 commit messages.
+  //     this.setState({ commits })
+  //   })
+  // }
+
   render() {
     let commitListElements = this.state.commits.map(commit => {
       return (<div class='Commit-container'>
