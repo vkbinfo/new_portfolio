4d3e2bab610c3d8ec6974c94ad0031d40e9662d3
Vikash Kumar Bijarnia <vkb.info@gmail.com>
2019-02-10 07:40:50 +0530

    Add Redirect for matched/no matched path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import HomeComponent from './home/homeComponent';
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
-    return <Route component={HomeComponent}></Route>;
+    return <Switch>
+      <Route path='/' component={HomeComponent}></Route>
+      <Redirect from='/' to='/' />
+    </Switch>;
   }
 }
 
